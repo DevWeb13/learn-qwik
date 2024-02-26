1fd0eea85d3c17e4a7a5cbd387cd549038c8bd04
DevWeb13 <giuliano.loic@sfr.fr>
2024-02-26 10:16:02 +0100

    fix: Delete double svg's file

--- src/routes/learn/dashboard-app/index.tsx
+++ src/routes/learn/dashboard-app/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from "@builder.io/qwik";
-import { DocumentHead } from "@builder.io/qwik-city";
+import { type DocumentHead } from "@builder.io/qwik-city";
 import HeaderOfMain from "~/components/headerOfMain/headerOfMain";
 import MobileMenu from "~/components/mobile-menu/mobile-menu";
 
