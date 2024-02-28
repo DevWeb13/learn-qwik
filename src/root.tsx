3890adb34e6982bc58b43538ae15ae3df46f8df1
DevWeb13 <giuliano.loic@sfr.fr>
2024-02-28 12:29:37 +0100

    Add partytown and Google tag manager test

--- src/root.tsx
+++ src/root.tsx
@@ -34,11 +34,11 @@ export default component$(() => {
           async
           dangerouslySetInnerHTML={`
           <!-- Google Tag Manager -->
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-M2HL6LDG');</script>
+          })(window,document,'script','dataLayer','GTM-M2HL6LDG');
           <!-- End Google Tag Manager -->
             `}
         />
@@ -49,13 +49,13 @@ export default component$(() => {
         ></script>
         <script
           dangerouslySetInnerHTML={`
-<!-- Google tag (gtag.js) -->
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+          <!-- Google tag (gtag.js) -->
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-  gtag('config', 'G-D5GX3GMZR7');
-`}
+            gtag('config', 'G-D5GX3GMZR7');
+          `}
         />
 
         <link rel="manifest" href="/manifest.json" />
