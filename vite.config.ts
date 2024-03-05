3df2d1605e1a9e601730a5577514b46b1a026b92
DevWeb13 <giuliano.loic@sfr.fr>
2024-03-05 22:44:11 +0100

    Delete partytown config

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { partytownVite } from "@builder.io/partytown/utils";
-import { join } from "path";
 
 export default defineConfig((): UserConfig => {
   return {
@@ -11,7 +9,7 @@ export default defineConfig((): UserConfig => {
       qwikCity(),
       qwikVite(),
       tsconfigPaths(),
-      partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
+      
     ],
     server: {
       headers: {
