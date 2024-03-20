63479d9d920b2a0d05a02accdbd6d26b77215335
DevWeb13 <giuliano.loic@sfr.fr>
2024-03-20 15:53:07 +0100

    test

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://www.learn-qwik.com/",
+        origin: "https://www.learn-qwik.com/learn",
       }),
     ],
   };
