45c4a2eb76bbcb84b9ea0561195f1b41c1478085
DevWeb13 <giuliano.loic@sfr.fr>
2024-03-20 15:35:22 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,16 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import rehypePrettyCode from 'rehype-pretty-code';
 
 export default defineConfig(async () => {
 
   return {
-    plugins: [qwikCity({
-      mdxPlugins: {
-          rehypeSyntaxHighlight: true,
-          remarkGfm: true,
-          rehypeAutolinkHeadings: true,
-      },
-     
-
-    }), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      
+    ],
     server: {
       headers: {
         "Cache-Control": "public, max-age=0",
