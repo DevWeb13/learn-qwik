0ba748d4a724f57f458591efc3f384b3ceadea96
DevWeb13 <giuliano.loic@sfr.fr>
2024-03-23 19:32:23 +0100

    revert

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -9,27 +9,17 @@ import {
   useSignal,
   useTask$,
 } from "@builder.io/qwik";
-import { routeAction$, routeLoader$, useLocation } from "@builder.io/qwik-city";
+import { routeLoader$, useLocation } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
 import Header from "../components/header/header";
 import Footer from "../components/starter/footer/footer";
-import type { ProgressCircle } from "~/types/types";
 
 export const MobileMenuVisibleContext = createContextId<Signal<boolean>>(
   "docs.mobile-menu-visible-context",
 );
 
-export const onGet: RequestHandler = async ({ cacheControl, cookie }) => {
-  let progressCircleCookie = cookie
-    .get("progressCircle")
-    ?.json<ProgressCircle>();
-  if (!progressCircleCookie) {
-    progressCircleCookie = {
-      completed: [],
-    };
-    cookie.set("progressCircle", progressCircleCookie, { path: "/" });
-  }
+export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
   // https://qwik.builder.io/docs/caching/
   cacheControl({
@@ -39,42 +29,6 @@ export const onGet: RequestHandler = async ({ cacheControl, cookie }) => {
     maxAge: 5,
   });
 };
-
-export const useGetProgressCircleCookie = routeLoader$(({ cookie }) => {
-  return cookie.get("progressCircle")?.json<ProgressCircle>();
-});
-
-export const useSetProgressCircleCookie = routeAction$(
-  async (data, requestEvent) => {
-    console.log(requestEvent);
-    const completedChapter = Number(data.goToChapter) - 1;
-    const nextUri = data.nextUri;
-    console.log({ completedChapter, nextUri });
-
-    let progressCircle = await requestEvent.cookie
-      .get("progressCircle")
-      ?.json<ProgressCircle>();
-
-    console.log({ progressCircle });
-
-    if (!progressCircle) {
-      progressCircle = {
-        completed: [],
-      };
-    }
-
-    if (
-      !progressCircle.completed.includes(completedChapter) &&
-      completedChapter !== 0
-    ) {
-      progressCircle.completed.push(completedChapter);
-      requestEvent.cookie.set("progressCircle", progressCircle, { path: "/" });
-    }
-
-    // redirect to the next chapter
-    requestEvent.redirect(302, `/learn/dashboard-app/${nextUri}`);
-  },
-);
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
