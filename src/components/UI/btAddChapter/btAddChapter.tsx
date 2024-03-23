0a911d502e9a42b0a4f6a6bcced58488190b6952
DevWeb13 <giuliano.loic@sfr.fr>
2024-03-23 19:32:56 +0100

    revert

--- src/components/UI/btAddChapter/btAddChapter.tsx
+++ src/components/UI/btAddChapter/btAddChapter.tsx
@@ -2,7 +2,7 @@
 
 import { component$ } from "@builder.io/qwik";
 import { Form } from "@builder.io/qwik-city";
-import { useSetProgressCircleCookie } from "~/routes/layout";
+import { useSetProgressCircleCookie } from "~/routes/learn/dashboard-app/layout";
 
 interface BtAddChapterProps {
   goToChapter: number;
