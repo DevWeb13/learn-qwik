840a97a0c6ea785e1ed2474d8eaa318cc39e3978
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-02 22:05:28 +0200

    conflict

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -6,17 +6,12 @@ export default extendConfig(baseConfig, () => {
   return {
     build: {
       ssr: true,
-      target: "node18.17.0",
       rollupOptions: {
         input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"],
         
       },
       outDir: ".vercel/output/functions/_qwik-city.func",
     },
-    plugins: [vercelEdgeAdapter(
-      {
-        target: "node",
-      } as VercelEdgeAdapterOptions,
-    )],
+    plugins: [vercelEdgeAdapter()],
   };
 });
