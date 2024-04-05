2ec1b1bab2676f42d7ea69f37992cd2a2bd2d8e1
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-05 17:21:54 +0200

    Setting sdynamic sitemap

--- src/routes/dynamic-sitemap.xml/index.tsx
+++ src/routes/dynamic-sitemap.xml/index.tsx
@@ -3,16 +3,16 @@ import { routes } from "@qwik-city-plan";
 import { createSitemap } from "./create-sitemap";
 
 export const onGet: RequestHandler = (ev) => {
-  const blogRoutes = routes
-    .map(([route]) => route as string)
-    .filter((route) => route.startsWith("/learn"));
+  // const blogRoutes = routes
+  //   .map(([route]) => route as string)
+  //   .filter((route) => route.startsWith("/"));
 
   const sitemap = createSitemap([
     { loc: "/", priority: 1 },
-    // { loc: "/learn", priority: 0.8 },
-    ...blogRoutes.map((route) => ({
+    { loc: "/learn", priority: 0.8 },
+    ...routes.map((route) => ({
       loc: route,
-      priority: 0.8,
+      priority: 0.7,
     })),
   ]);
 
