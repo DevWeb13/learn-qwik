8fd9a60c4ecf76ae66fadcc2a75d4de53c11645e
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-08 16:42:48 +0200

    disable qwik pwa

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -2,7 +2,7 @@ import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
 
 import { component$ } from "@builder.io/qwik";
 
-import * as pwaHead from "@qwikdev/pwa/head";
+// import * as pwaHead from "@qwikdev/pwa/head";
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
@@ -21,12 +21,12 @@ export const RouterHead = component$(() => {
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
-      {pwaHead.meta.map((l) => (
+      {/* {pwaHead.meta.map((l) => (
         <meta key={l.key} {...l} />
       ))}
       {pwaHead.links.map((l) => (
         <link key={l.key} {...l} />
-      ))}
+      ))} */}
 
       {head.links.map((l) => (
         <link key={l.key} {...l} />
