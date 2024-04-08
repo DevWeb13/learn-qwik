68ff20aeda9df9346f389c4129234387d57a9bca
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-08 18:04:23 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,24 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-// import { qwikPwa } from "@qwikdev/pwa";
+import { type PWAOptions, qwikPwa } from "@qwikdev/pwa";
 
+const config: PWAOptions | undefined = process.env.CUSTOM_CONFIG === "true"
+ ? { config: true }
+    : undefined;
 
-export default defineConfig(async () => {
-
+export default defineConfig(() => {
   return {
+    define: {
+      // enables debugging in workbox
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    },
     plugins: [
       qwikCity(),
       qwikVite(),
       tsconfigPaths(),
-      // qwikPwa(),
+      qwikPwa(config)
     ],
-    server: {
-      headers: {
-        "Cache-Control": "public, max-age=0",
-      },
-    },
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
