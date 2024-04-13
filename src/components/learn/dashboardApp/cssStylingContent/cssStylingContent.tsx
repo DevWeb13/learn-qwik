4b7eba5172d55b90520630dc6a0f2d467e1b9d37
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-13 18:01:55 +0200

    Active chapter 3 button

--- src/components/learn/dashboardApp/cssStylingContent/cssStylingContent.tsx
+++ src/components/learn/dashboardApp/cssStylingContent/cssStylingContent.tsx
@@ -811,7 +811,6 @@ export const HiArrowRightOutline = component$<HiArrowRightOutlineProps>(
           goToChapter={3}
           title="Optimizing Fonts and Images"
           text="Continue working on your home page by adding a hero image and a custom font."
-          disabledButton={true}
         />
       </div>
       <Feedback />
