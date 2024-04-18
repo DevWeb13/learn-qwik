f39c828cb4c52cb5724516e6545b80fadbccdd11
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-18 12:52:38 +0200

    Finish chapter 3

--- src/components/learn/dashboardApp/optimizing-fonts-and-images-content/optimizing-fonts-and-images-content.tsx
+++ src/components/learn/dashboardApp/optimizing-fonts-and-images-content/optimizing-fonts-and-images-content.tsx
@@ -12,13 +12,17 @@ import CodeBlock from "~/components/UI/codeBlock/codeBlock";
 import { EyeSvg } from "~/assets/svg/eyeSvg/eyeSvg";
 import { EyeBarredSvg } from "~/assets/svg/eyeBarred/eyeBarred";
 import { FontSoluce } from "./fontSoluce/fontSoluce";
+import { Quiz } from "~/components/UI/quiz/quiz";
 
 import AddLusitanaAndLRDQwikLogoImg from "~/assets/img/addLusitanaAndLRDQwikLogo.png?jsx";
 import AddInterImg from "~/assets/img/addInterImg.png?jsx";
-import { Quiz } from "~/components/UI/quiz/quiz";
+import CLSImg from "~/assets/img/CLS.png?jsx";
+import AddDesktopHeroImg from "~/assets/img/addDesktopHeroImg.png?jsx";
+import { ImageSoluce } from "./imageSoluce/imageSoluce";
 
 export default component$(() => {
   const fontSoluceDisplay = useSignal(false);
+  const imageSoluceDisplay = useSignal(false);
   useStyles$(``);
   return (
     <article
@@ -88,6 +92,31 @@ export default component$(() => {
           once loaded. This change can affect the size, spacing, or layout of
           the text, influencing other elements on the page.
         </p>
+        <figure>
+          <CLSImg
+            alt="Mock UI showing initial load of a page, followed by a layout shift as the custom font loads."
+            class="block rounded-md border border-gray-200 bg-gray-100 "
+          />
+        </figure>
+
+        <p>
+          In Qwik, you can optimize font loading by using the{" "}
+          <code>`font-display`</code> property to control how fonts are loaded.
+          Common values for this property include <code>`swap`</code> and{" "}
+          <code>`fallback`</code>, allowing you to choose between different
+          loading strategies.
+        </p>
+        <p>
+          To further improve performance, Qwik recommends self-hosting your
+          fonts. Instead of using third-party providers like Google Fonts, you
+          can download the font files and serve them from your own domain, which
+          reduces network requests and enhances privacy as well as performance,
+          especially in PWA scenarios or under limited connectivity.
+        </p>
+        <p>
+          This ensures that the custom font is used efficiently, minimizing
+          impacts on CLS and enhancing the overall user experience.
+        </p>
         <figure class="flex flex-col items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-3 pt-8">
           <video
             autoplay
@@ -112,24 +141,6 @@ export default component$(() => {
             intended to cancel.
           </p>
         </figure>
-        <p>
-          In Qwik, you can optimize font loading by using the{" "}
-          <code>`font-display`</code> property to control how fonts are loaded.
-          Common values for this property include <code>`swap`</code> and{" "}
-          <code>`fallback`</code>, allowing you to choose between different
-          loading strategies.
-        </p>
-        <p>
-          To further improve performance, Qwik recommends self-hosting your
-          fonts. Instead of using third-party providers like Google Fonts, you
-          can download the font files and serve them from your own domain, which
-          reduces network requests and enhances privacy as well as performance,
-          especially in PWA scenarios or under limited connectivity.
-        </p>
-        <p>
-          This ensures that the custom font is used efficiently, minimizing
-          impacts on CLS and enhancing the overall user experience.
-        </p>
         <Quiz
           question="Why is it recommended to self-host fonts in a Qwik application?"
           options={[
@@ -530,41 +541,264 @@ export default component$(() => {
           <span class="font-bold">Congratulations!</span> You have successfully
           added custom fonts to your Qwik application.🎉
         </p>
-        {/* <SubtitleWithAnchor title="Optimizing Images" id="optimizing-images" />
+        <SubtitleWithAnchor
+          title="Why optimize images?"
+          id="why-optimize-images?"
+        />
+
         <p>
-          Images are essential for web design, but they can also impact
-          performance. To optimize images, you can use the <code>`srcset`</code>{" "}
-          attribute to provide different image sizes based on the device's
-          resolution.
+          In Qwik, optimizing images is crucial for reducing file sizes and
+          improving loading performance on networks, especially mobile ones.
+          Qwik integrates this functionality through the vite-imagetools module,
+          allowing automated management without the need for additional
+          packages.
         </p>
+
+        <SubtitleWithAnchor
+          title="How does it work with Qwik?"
+          id="how-does-it-work-with-qwik?"
+        />
+
+        <ol class="ml-1">
+          <li>
+            <span class="mr-2 font-bold text-[--qwik-dark-blue]">1.</span>
+            <span class="font-bold">Importing images: </span>You can import an
+            image from the <code>src</code> folder. This image is automatically
+            converted into multiple formats (WebP, AVIF) and sizes (200px,
+            400px, 600px, 800px, 1200px) suitable for different breakpoints.
+          </li>
+          <li>
+            <span class="mr-2 font-bold text-[--qwik-dark-blue]">2.</span>
+            <span class="font-bold">
+              Rendering the <code>{`<img>`}</code> Element:{" "}
+            </span>
+            An{" "}
+            <span class="font-bold">
+              <code>{`<img>`}</code>
+            </span>{" "}
+            element is generated with the <code>srcset</code> attribute to
+            specify different image sources depending on the resolution used by
+            the browser. This allows the most appropriate image to be loaded
+            depending on the device's resolution.
+          </li>
+          <li>
+            <span class="mr-2 font-bold text-[--qwik-dark-blue]">3.</span>
+            <span class="font-bold">Image Attributes: </span>Images are loaded
+            lazily (<code>loading="lazy"</code>) and asynchronous decoding (
+            <code>decoding="async"</code>) is used to not block the page's
+            rendering while the image is being decoded.
+          </li>
+          <li>
+            <span class="mr-2 font-bold text-[--qwik-dark-blue]">4.</span>
+            <span class="font-bold">Suffix: </span>Add the <code>?jsx</code>{" "}
+            suffix at the end of the image path to enable image optimization.
+          </li>
+        </ol>
+
+        <SubtitleWithAnchor
+          title="Adding the desktop hero image"
+          id="adding-the-desktop-hero-image"
+        />
+
         <p>
-          This ensures that the browser only downloads the image size it needs,
-          reducing the image's file size and improving performance.
+          Let's add a hero image to your dashboard. This image will be displayed
+          on larger screens.
         </p>
+
         <p>
-          In Qwik, you can use the <code>{`<img>`}</code> tag with the{" "}
-          <code>`srcset`</code> attribute to optimize images.
+          First<span class="font-bold"> download</span> the hero desktop image
+          here:
         </p>
+        <ul>
+          <li>
+            {" "}
+            <a href="/downloads/heroImg.png" download="heroImg.png">
+              <code>heroImg.png 💾</code>
+            </a>
+          </li>
+        </ul>
+
         <p>
-          Let's add a hero image to your dashboard using the{" "}
-          <code>{`<img>`}</code> tag with the <code>`srcset`</code> attribute.
+          In your <code>src/assets/</code> folder, create a new folder called{" "}
+          <code>img</code> and add the hero image there.
         </p>
+
         <p>
-          First, download the image from{" "}
+          In your <code>src/routes/index.tsx</code> file, import the hero image,
+          don't forget to add the <code>?jsx</code> suffix at the end of the
+          image path.
+        </p>
+
+        <p>Next, add the hero image to your file:</p>
+
+        <CodeBlock
+          code={`import { component$ } from "@builder.io/qwik";
+import { Link, type DocumentHead } from "@builder.io/qwik-city";
+import { HiArrowRightOutline } from "~/assets/svg/HiArrowRightOutline";
+import styles from "./home.module.css";
+
+import { LRDQwikLogo } from "~/assets/svg/LRDQwikLogo";
+
+import HeroImg from "~/assets/img/heroImg.png?jsx";
+
+export default component$(() => {
+  return (
+    <main class="flex min-h-screen flex-col p-6">
+      <div class="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+        <LRDQwikLogo />
+      </div>
+      <div class="mt-4 flex grow flex-col gap-4 md:flex-row">
+      // ...
+        <div class="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/* Add Hero Images Here */}
+          <HeroImg class="hidden md:block" />
+        </div>
+      </div>
+    </main>
+  );
+});`}
+          language="tsx"
+          text="src/routes/index.tsx"
+          icon="typescript"
+          decorations={[
+            {
+              start: { line: 7, character: 0 },
+              end: { line: 7, character: 51 },
+              properties: { class: "newLine" },
+            },
+            {
+              start: { line: 19, character: 0 },
+              end: { line: 19, character: 45 },
+              properties: { class: "newLine" },
+            },
+          ]}
+        />
+
+        <p class="dark-plus">
+          <span>⚠️</span>{" "}
+          <span>
+            The <code>'hidden md:block'</code> class ensures the image is only
+            displayed on larger screens.
+          </span>
+        </p>
+
+        <p>This is what your home page should look like now:</p>
+
+        <figure class="flex items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-3">
+          <AddDesktopHeroImg
+            alt="Desktop hero image displayed on the home page."
+            class="block w-full rounded-md border border-gray-200 bg-gray-100"
+          />
+        </figure>
+
+        <SubtitleWithAnchor
+          title="Practice: Adding the mobile hero image"
+          id="practice:-adding-the-mobile-hero-image"
+        />
+
+        <p>
+          Now it's your turn! Add a mobile hero image to your dashboard. This is
+          the image that will be displayed on smaller screens.
+        </p>
+
+        <p>
+          You can<span class="font-bold"> download</span> the hero mobile image
+          here:
+        </p>
+        <ul>
+          <li>
+            {" "}
+            <a href="/downloads/heroMobileImg.png" download="heroMobileImg.png">
+              <code>heroMobileImg.png 💾</code>
+            </a>
+          </li>
+        </ul>
+
+        <p>
+          Once you're ready, expand the code snippet below to see the solution.
+        </p>
+
+        <div class="bg-vercel-200 -mx-5 mb-8 p-[21px] md:-mx-[62px] md:rounded-[16px] md:p-4 md:px-[62px] md:py-12">
+          <button
+            class="button_base reset_reset button_button  geist-new-themed geist-new-button geist-new-button-fill button_invert"
+            data-geist-button=""
+            data-prefix="true"
+            data-suffix="false"
+            data-version="v1"
+            style="--geist-icon-size: 16px;"
+            onClick$={() =>
+              (imageSoluceDisplay.value = !imageSoluceDisplay.value)
+            }
+          >
+            <span class="button_prefix">
+              {imageSoluceDisplay.value ? <EyeBarredSvg /> : <EyeSvg />}
+            </span>
+            <span class="button_content">
+              {imageSoluceDisplay.value
+                ? "Hide the solution"
+                : "Reveal the solution"}
+            </span>
+          </button>
+          {imageSoluceDisplay.value && <ImageSoluce />}
+        </div>
+
+        <p>
+          <span class="font-bold">Great!</span> Your home page now has a custom
+          font and hero images.
+        </p>
+
+        <Quiz
+          question="Using the .jsx suffix when importing images in Qwik enables automatic optimization, including conversion to modern formats and lazy loading of images."
+          options={[
+            {
+              text: "True",
+              isCorrect: true,
+              letter: "A",
+            },
+            {
+              text: "False",
+              isCorrect: false,
+              letter: "B",
+            },
+          ]}
+          hint="Remember to review how the .jsx suffix affects image processing in Qwik. This suffix is crucial for activating certain automatic optimizations."
+          responseText="When you use the .jsx suffix in Qwik, it activates the vite-imagetools which automatically handles image optimization. This includes converting images to more efficient formats like WebP and AVIF, and implementing lazy loading to improve page load times. This is essential for enhancing performance and user experience on modern websites."
+        />
+
+        <SubtitleWithAnchor
+          title="Recommended reading"
+          id="recommended reading"
+        />
+
+        <p>
+          There's a lot more to learn about these topics. If you'd like to dive
+          deeper into fonts and images, see:{" "}
+        </p>
+
+        <ul>
+          <li>
+            <BlankLink
+              href="https://qwik.dev/docs/cookbook/fonts/"
+              text="Qwik Docs Cookbook Fonts"
+            />
+          </li>
+
+          <li>
+            <BlankLink
+              href="https://qwik.dev/docs/integrations/image-optimization/"
+              text="Qwik Docs Image Optimization"
+            />
+          </li>
+        </ul>
+
+        <p>
+          You can find the source code for chapter 3 on{" "}
           <BlankLink
-            href="https://unsplash.com/photos/6H9H8d4ode4"
-            text="Unsplash"
+            href="https://github.com/DevWeb13/qwik-dashboard/tree/2-chapter-3-optimizing-fonts-and-images"
+            text="GitHub"
           />
           .
         </p>
-        <p>
-          Next, add the image to your project in the <code>src/assets/</code>{" "}
-          directory.
-        </p>
-        <p>
-          In your <code>src/routes/index.tsx</code> file, add the following code
-          to display the image:
-        </p> */}
       </div>
 
       <div class="relative mx-auto mb-8 mt-4 flex w-full max-w-[640px] flex-col items-center md:my-20 md:mt-12">
