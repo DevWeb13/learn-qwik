6fcd24ecf3539d5008f49b9c39e6c9f55ddf2114
DevWeb13 <giuliano.loic@sfr.fr>
2024-05-06 15:04:30 +0200

    create API layer section

--- src/components/learn/dashboardApp/fetchingDataContent/fetchingDataContent.tsx
+++ src/components/learn/dashboardApp/fetchingDataContent/fetchingDataContent.tsx
@@ -1,7 +1,9 @@
 // src/components/learn/dashboardApp/fetchingDataContent/fetchingDataContent.tsx
 
 import { component$, useStyles$ } from "@builder.io/qwik";
+import BlankLink from "~/components/UI/blankLink/blankLink";
 import PageTitle from "~/components/UI/pageTitle/pageTitle";
+import SubtitleWithAnchor from "~/components/UI/subtitleWithAnchor/subtitleWithAnchor";
 import TableOfTopicsCovered from "~/components/UI/tableOfTopicsCovered/tableOfTopicsCovered";
 
 export const FetchingDataContent = component$(() => {
@@ -38,6 +40,33 @@ export const FetchingDataContent = component$(() => {
             },
           ]}
         />
+
+        <SubtitleWithAnchor
+          title="Choosing how to fetch data"
+          id="choosing-how-to-fetch-data"
+        />
+
+        <SubtitleWithAnchor title="API layer" id="api-layer" level="h3" />
+
+        <p>
+          APIs are an intermediary layer between your application code and
+          database. There are a few cases where you might use an API:
+        </p>
+
+        <ul>
+          <li>If you're using 3rd party services that provide an API.</li>
+          <li>
+            If you're fetching data from the client, you want to have an API
+            layer that runs on the server to avoid exposing your database
+            secrets to the client.
+          </li>
+        </ul>
+
+        <p>
+          In Qwik, you can create API endpoints using{" "}
+          <BlankLink href="https://qwik.dev/docs/endpoints/" text="Endpoints" />
+          .
+        </p>
       </div>
     </>
   );
