01f0ade2f53dad080b05627a3887baf812f72d42
DevWeb13 <giuliano.loic@sfr.fr>
2024-05-17 20:29:59 +0200

    Create <OptimizingDataFetch />

---
+++ src/components/learn/dashboardApp/optimizingDataFetch/optimizingDataFetch.tsx
@@ -0,0 +1,7 @@
+// src/components/learn/dashboardApp/optimizingDataFetch/optimizingDataFetch.tsx
+
+import { component$ } from "@builder.io/qwik";
+
+export const OptimizingDataFetch = component$(() => {
+  return <div>OptimizingDataFetch</div>;
+});
