b43329f2ea3c78561cef168546c30874c4935308
DevWeb13 <giuliano.loic@sfr.fr>
2024-05-18 15:29:37 +0200

    Add <TableOfTopicsCovered />

--- src/components/learn/dashboardApp/optimizingDataFetch/optimizingDataFetch.tsx
+++
@@ -1,7 +0,0 @@
-// src/components/learn/dashboardApp/optimizingDataFetch/optimizingDataFetch.tsx
-
-import { component$ } from "@builder.io/qwik";
-
-export const OptimizingDataFetch = component$(() => {
-  return <div>OptimizingDataFetch</div>;
-});
