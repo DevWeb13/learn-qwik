470a97cf830b1b543920fea41fcaa8c75688acc3
DevWeb13 <giuliano.loic@sfr.fr>
2024-05-29 21:19:49 +0200

    Configurer les Redirections de Manière Optimale : Utilisez des redirections 301 pour indiquer des changements permanents aux moteurs de recherche.

--- src/routes/learn/index.tsx
+++ src/routes/learn/index.tsx
@@ -2,5 +2,5 @@
 import type { RequestEvent } from "@builder.io/qwik-city";
 
 export const onGet = async ({ redirect }: RequestEvent) => {
-  throw redirect(302, "/");
+  throw redirect(301, "/");
 };
