8655e6890d3158fb7327fea0b9ff5bbaf1f6ffc3
DevWeb13 <giuliano.loic@sfr.fr>
2024-07-19 11:35:50 +0200

    Ads in loader

--- src/components/UI/loader/loader.tsx
+++ src/components/UI/loader/loader.tsx
@@ -41,6 +41,21 @@ export const Loader = component$(() => {
   return (
     <main class="loader">
       <Logo />
+      <div class="px-4 pb-8 md:px-8 md:pb-20">
+        <ins
+          class="adsbygoogle"
+          style="display:flex; text-align:center; justify-content:center;"
+          data-ad-format="autorelaxed"
+          data-ad-client="ca-pub-2091224773462896"
+          data-ad-slot="2125459059"
+        ></ins>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={`
+  (adsbygoogle = window.adsbygoogle || []).push({});
+`}
+        />
+      </div>
     </main>
   );
 });
