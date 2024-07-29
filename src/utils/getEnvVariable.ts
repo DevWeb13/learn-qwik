9d98d9bbae8e7ae73fd046e473a9244f6e680203
DevWeb13 <giuliano.loic@sfr.fr>
2024-07-29 18:44:03 +0200

    Add console.log

--- src/utils/getEnvVariable.ts
+++ src/utils/getEnvVariable.ts
@@ -2,5 +2,6 @@ import { server$ } from "@builder.io/qwik-city";
 
 export const getEnvVariable = server$(function (envVariableName: string) {
   const envValue = this.env.get(envVariableName);
+  console.log(`Env variable ${envVariableName}:`, envValue);
   return envValue;
 });
