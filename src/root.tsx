724b66aad835ce67a1f429361a6e256e3cfa0ec2
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-05 16:10:14 +0200

    Test deploiement with prompts to reauthorize ads

--- src/root.tsx
+++ src/root.tsx
@@ -99,6 +99,16 @@ export default component$(() => {
           crossOrigin="anonymous"
         />
 
+        <script
+          async
+          src="https://fundingchoicesmessages.google.com/i/pub-2091224773462896?ers=1"
+          nonce="t22S2X3kbCjPhU4tQwlBJA"
+        />
+        <script
+          nonce="t22S2X3kbCjPhU4tQwlBJA"
+          dangerouslySetInnerHTML={`(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`}
+        />
+
         {/* Google tag (gtag.js) Start */}
         <script
           async
