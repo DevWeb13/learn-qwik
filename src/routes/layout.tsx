2386f3c4e6a1a6b6127d4d34f421d4bd6726bd3f
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-08 13:03:23 +0200

    Refactor reinit adsense

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -168,19 +168,14 @@ export default component$(() => {
     track(() => location.isNavigating);
     // reinit adsense for spa navigation
     console.log("reinit adsense");
-    const adsbygoogles = container.value?.querySelectorAll(".adsbygoogle");
-    adsbygoogles?.forEach((adsbygoogle) => {
-      adsbygoogle.setAttribute("data-adsbygoogle-status", ""); // Réinitialiser l'attribut de statut
-      adsbygoogle.setAttribute("data-ad-status", ""); // Réinitialiser l'attribut de statut
-      adsbygoogle.innerHTML = ""; // Supprimer le contenu de l'élément
+
+    // @ts-ignore
+    if (typeof window !== "undefined" && window.adsbygoogle) {
       // @ts-ignore
-      if (typeof window !== "undefined" && window.adsbygoogle) {
-        // @ts-ignore
-        window.adsbygoogle = window.adsbygoogle || [];
-        // @ts-ignore
-        window.adsbygoogle.push({});
-      }
-    });
+      window.adsbygoogle = window.adsbygoogle || [];
+      // @ts-ignore
+      window.adsbygoogle.push({});
+    }
   });
 
   return (
