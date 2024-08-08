5e2158a550cfb0a3f0912c01ff3e3ce87631c541
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-08 10:46:16 +0200

    add partytown all script

--- src/root.tsx
+++ src/root.tsx
@@ -28,26 +28,25 @@ export default component$(() => {
 
         {/* Google Tag Manager */}
         <script
+          type="text/partytown"
           async
-          defer
           dangerouslySetInnerHTML={`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.defer=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M2HL6LDG');
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M2HL6LDG');
           `}
         />
 
         {/* Google Analytics */}
         <script
+          type="text/partytown"
           async
-          defer
           src="https://www.googletagmanager.com/gtag/js?id=G-D5GX3GMZR7"
         ></script>
         <script
-          async
-          defer
+          type="text/partytown"
           dangerouslySetInnerHTML={`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -58,19 +57,20 @@ export default component$(() => {
 
         {/* Google AdSense */}
         <script
+          type="text/partytown"
           async
-          defer
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2091224773462896"
           crossOrigin="anonymous"
         />
 
         {/* Google Funding Choices */}
         <script
+          type="text/partytown"
           async
-          defer
           src="https://fundingchoicesmessages.google.com/i/pub-2091224773462896?ers=1"
         />
         <script
+          type="text/partytown"
           dangerouslySetInnerHTML={`
             (function() {
               function signalGooglefcPresent() {
