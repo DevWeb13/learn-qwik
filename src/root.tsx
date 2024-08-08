35409116a058ca0e856f7a2bf609731956be2ed0
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-08 14:39:15 +0200

    partytown google tag

--- src/root.tsx
+++ src/root.tsx
@@ -29,29 +29,31 @@ export default component$(() => {
         {/* Google Tag Manager */}
         <script
           async
-          dangerouslySetInnerHTML={`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-M2HL6LDG');
-          `}
-        />
-
-        {/* Google Analytics */}
-        <script
-          async
+          type="text/partytown"
           src="https://www.googletagmanager.com/gtag/js?id=G-D5GX3GMZR7"
         />
         <script
+          type="text/partytown"
           dangerouslySetInnerHTML={`
             window.dataLayer = window.dataLayer || [];
             window.gtag = function() {
               dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
+              }
+              gtag('js', new Date());
             gtag('config', 'G-D5GX3GMZR7');
           `}
+        />
+
+        {/* Google Analytics ? */}
+        <script
+          async
+          dangerouslySetInnerHTML={`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-M2HL6LDG');
+                `}
         />
 
         {/* Google AdSense */}
@@ -68,6 +70,7 @@ export default component$(() => {
         />
 
         <script
+          type="text/partytown"
           async
           dangerouslySetInnerHTML={`
             (function() {
