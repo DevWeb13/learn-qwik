12a51faabc34989f53c20a5bcb9499b390240a04
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-08 09:27:54 +0200

    Add partytown all script

--- src/root.tsx
+++ src/root.tsx
@@ -7,11 +7,10 @@ import {
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
-// import { QwikPartytown } from "./components/partytown/partytown";
+import { QwikPartytown } from "./components/partytown/partytown";
 
 import "./global.css";
 import "./button.css";
-import { QwikPartytown } from "./components/partytown/partytown";
 
 export const runtime = "node";
 
@@ -20,13 +19,6 @@ const isProd = process.env.NODE_ENV === "production";
 console.log("isProd", isProd);
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
-
   return (
     <QwikCityProvider>
       <head>
@@ -34,21 +26,20 @@ export default component$(() => {
 
         <QwikPartytown forward={["dataLayer.push"]} />
 
-        {/* Google tag (gtag.js) Start */}
+        {/* Google Tag Manager */}
         <script
           type="text/partytown"
           async
           dangerouslySetInnerHTML={`
-          <!-- Google Tag Manager -->
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-M2HL6LDG');
-          <!-- End Google Tag Manager -->
-            `}
+          `}
         />
 
+        {/* Google Analytics */}
         <script
           type="text/partytown"
           async
@@ -57,18 +48,51 @@ export default component$(() => {
         <script
           type="text/partytown"
           dangerouslySetInnerHTML={`
-          <!-- Google tag (gtag.js) -->
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-D5GX3GMZR7');
+          `}
+        />
+
+        {/* Google AdSense */}
+        <script
+          type="text/partytown"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2091224773462896"
+          crossOrigin="anonymous"
+        />
+
+        {/* Google Funding Choices */}
+        <script
+          type="text/partytown"
+          async
+          src="https://fundingchoicesmessages.google.com/i/pub-2091224773462896?ers=1"
+        />
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={`
+            (function() {
+              function signalGooglefcPresent() {
+                if (!window.frames['googlefcPresent']) {
+                  if (document.body) {
+                    const iframe = document.createElement('iframe');
+                    iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+                    iframe.style.display = 'none';
+                    iframe.name = 'googlefcPresent';
+                    document.body.appendChild(iframe);
+                  } else {
+                    setTimeout(signalGooglefcPresent, 0);
+                  }
+                }
+              }
+              signalGooglefcPresent();
+            })();
           `}
         />
 
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
-
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -86,37 +110,12 @@ export default component$(() => {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-
         <link rel="mask-icon" href="/safari-pinned-tab.svg" />
         <meta name="apple-mobile-web-app-title" content="Learn Qwik" />
         <meta name="application-name" content="Learn Qwik" />
         <meta name="msapplication-TileColor" content="#2d89ef" />
         <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
-
-        {/* Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2091224773462896"
-          crossOrigin="anonymous"
-        />
-
-        <script
-          async
-          src="https://fundingchoicesmessages.google.com/i/pub-2091224773462896?ers=1"
-          nonce="t22S2X3kbCjPhU4tQwlBJA"
-        />
-        <script
-          nonce="t22S2X3kbCjPhU4tQwlBJA"
-          dangerouslySetInnerHTML={`(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`}
-        />
-
-        {/* Cool Qwik trick to know what code is being executed and when 👇 */}
-        {/* {!isProd && (
-          <script
-            dangerouslySetInnerHTML={`document.addEventListener('qsymbol', (e) => console.log(e.detail));`}
-          />
-        )} */}
 
         <RouterHead />
         <ServiceWorkerRegister />
