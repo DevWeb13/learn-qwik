ec783374823c86d23eb78816eaea2ba4b5a59e70
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-13 23:01:20 +0200

    Fix arrow icons link blocked => Change to SVG

--- src/components/UI/SocialShareButtons/ButtonHandleShare.tsx
+++ src/components/UI/SocialShareButtons/ButtonHandleShare.tsx
@@ -19,21 +19,33 @@ export const ButtonHandleShare = component$<ButtonHandleShareProps>(
       >
         {buttonHandleShareStore.isOpen ? (
           <div class="st-left">
-            <img
-              alt="arrow_left sharing button"
-              src="https://platform-cdn.sharethis.com/img/arrow_left.svg"
-              width={24}
-              height={24}
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#fff"
+              preserveAspectRatio="xMidYMid meet"
+              height="24px"
+              width="24px"
+              viewBox="0 0 40 40"
+            >
+              <g>
+                <path d="m22 30.7q0 0.3-0.2 0.5l-1.1 1.1q-0.3 0.3-0.6 0.3t-0.5-0.3l-10.4-10.4q-0.2-0.2-0.2-0.5t0.2-0.5l10.4-10.4q0.3-0.2 0.5-0.2t0.6 0.2l1.1 1.1q0.2 0.3 0.2 0.5t-0.2 0.6l-8.8 8.7 8.8 8.8q0.2 0.2 0.2 0.5z m8.6 0q0 0.3-0.3 0.5l-1.1 1.1q-0.2 0.3-0.5 0.3t-0.5-0.3l-10.4-10.4q-0.2-0.2-0.2-0.5t0.2-0.5l10.4-10.4q0.2-0.2 0.5-0.2t0.5 0.2l1.1 1.1q0.3 0.3 0.3 0.5t-0.3 0.6l-8.7 8.7 8.7 8.8q0.3 0.2 0.3 0.5z" />
+              </g>
+            </svg>
           </div>
         ) : (
           <div class="st-right">
-            <img
-              alt="arrow_right sharing button"
-              src="https://platform-cdn.sharethis.com/img/arrow_right.svg"
-              width={24}
-              height={24}
-            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#fff"
+              preserveAspectRatio="xMidYMid meet"
+              height="24px"
+              width="24px"
+              viewBox="0 0 40 40"
+            >
+              <g>
+                <path d="m22.3 21.4q0 0.3-0.2 0.5l-10.4 10.4q-0.3 0.3-0.6 0.3t-0.5-0.3l-1.1-1.1q-0.2-0.2-0.2-0.5t0.2-0.5l8.8-8.8-8.8-8.7q-0.2-0.3-0.2-0.6t0.2-0.5l1.1-1.1q0.3-0.2 0.5-0.2t0.6 0.2l10.4 10.4q0.2 0.2 0.2 0.5z m8.6 0q0 0.3-0.3 0.5l-10.4 10.4q-0.2 0.3-0.5 0.3t-0.5-0.3l-1.1-1.1q-0.2-0.2-0.2-0.5t0.2-0.5l8.8-8.8-8.8-8.7q-0.2-0.3-0.2-0.6t0.2-0.5l1.1-1.1q0.2-0.2 0.5-0.2t0.5 0.2l10.4 10.4q0.3 0.2 0.3 0.5z" />
+              </g>
+            </svg>
           </div>
         )}
       </button>
