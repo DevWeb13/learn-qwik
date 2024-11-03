06c6390dd74fe3b5f3fae798cef09b9e49ddd3c1
DevWeb13 <giuliano.loic@sfr.fr>
2024-11-03 16:59:42 +0100

    add blog page to sitemap

--- src/routes/dynamic-sitemap.xml/index.tsx
+++ src/routes/dynamic-sitemap.xml/index.tsx
@@ -5,20 +5,30 @@ import { routes } from "@qwik-city-plan";
 import { createSitemap } from "./create-sitemap";
 
 export const onGet: RequestHandler = (ev) => {
-  // console.log({ routes });
-
   const learnRoutes = routes
     .map(([route]) => route as string)
-    .filter((route) => route.startsWith("learn/dashboard-app"));
+    .filter((route) => route.startsWith("learn/dashboard-app/"));
 
-  const privacyPolicy = "/privacy";
+  const privacyPolicy = "/privacy/";
   learnRoutes.push(privacyPolicy);
 
+  // Définissez le nombre de pages de blog dynamiquement
+  const totalBlogPages = 30; // ou une valeur récupérée dynamiquement
+  const blogPages = Array.from(
+    { length: totalBlogPages },
+    (_, i) => `/blog/?page=${i + 1}`,
+  );
+
+  // Créez le sitemap avec toutes les routes
   const sitemap = createSitemap([
     { loc: "/", priority: 1 },
     ...learnRoutes.map((route) => ({
       loc: route,
       priority: 0.9,
+    })),
+    ...blogPages.map((page) => ({
+      loc: page,
+      priority: 0.8, // Priorité plus faible pour les pages de blog si nécessaire
     })),
   ]);
 
