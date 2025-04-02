b7be4bfed07bae4dd6d2bd661d6f4ca41f6f7435
DevWeb13 <giuliano.loic@sfr.fr>
2025-04-02 12:08:20 +0200

    Supprime l'appel à la fonction `updateExpiredSubscriptions` dans le middleware pour éviter les mises à jour automatiques des abonnements expirés.

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -5,7 +5,7 @@ import { SupabaseClient, User } from "@supabase/supabase-js";
 // import { CHAPTERS } from "~/constants/chapters";
 import { createClient } from "~/lib/supabase/server";
 import { Database } from "~/types/database.types";
-import { isSubscriptionActive, updateExpiredSubscriptions } from "~/utils/subscription";
+import { isSubscriptionActive } from "~/utils/subscription";
 
 // const CHAPTERS_FREE_LIMIT = 6;
 
@@ -60,7 +60,7 @@ export async function updateSession(requestEvent: RequestEvent) {
     }
 
     // Vérifier et mettre à jour les abonnements expirés
-    await updateExpiredSubscriptions(requestEvent);
+    // await updateExpiredSubscriptions(requestEvent);
 
     const user = await getUser(supabase, requestEvent);
     
