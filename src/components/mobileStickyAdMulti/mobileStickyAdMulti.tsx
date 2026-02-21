6113511c70896b3149e96888dbfa77c0aa0fcc8c
DevWeb13 <giuliano.loic@sfr.fr>
2026-02-21 14:06:44 +0100

    fix: Adjust mobile sticky ad dimensions for better responsiveness

--- src/components/mobileStickyAdMulti/mobileStickyAdMulti.tsx
+++ src/components/mobileStickyAdMulti/mobileStickyAdMulti.tsx
@@ -5,10 +5,10 @@ import { component$ } from "@builder.io/qwik";
 export const MobileStickyAdMulti = component$(() => {
   return (
     <aside class="fixed bottom-12 left-0 right-0 z-50 block bg-white shadow-inner lg:hidden">
-      <div class="mx-auto max-w-[360px]">
+      <div class="mx-auto">
         <ins
           class="adsbygoogle"
-          style="display:inline-block;width:320px;height:100px"
+          style="display:inline-block;height:100px"
           data-ad-client="ca-pub-2091224773462896"
           data-ad-slot="3249947732"
         ></ins>
